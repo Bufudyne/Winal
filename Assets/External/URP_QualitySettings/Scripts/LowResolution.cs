75d7e27ed852d95a4cb2b1627d097e3992a2a368
Atazio <alejandro.arana@tortasd.local>
2022-05-14 04:12:33 -0400

    Screen resolution settings

---
+++ Assets/External/URP_QualitySettings/Scripts/LowResolution.cs
@@ -0,0 +1,61 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class LowResolution : MonoBehaviour
+{
+	
+	public bool isFirstScene = true;
+
+
+	void Start ()
+	{
+		#region Default Settings
+
+		// Set the game default setting when it is the first run on the device
+		if (PlayerPrefs.GetInt("The First Run") != 1) // 1 = true; others = 0
+		{
+			// Set the default quality level
+			PlayerPrefs.SetInt("Quality Level", 0);
+
+			// Store the device original resolution
+			PlayerPrefs.SetInt("OriginalX", Screen.width);
+
+			PlayerPrefs.SetInt("OriginalY", Screen.height);
+			
+			//Resolution Settings
+			PlayerPrefs.SetInt("Resolution Quality", 2);
+
+			// The is not the first run anymore
+			PlayerPrefs.SetInt("The First Run", 1);
+		}
+		
+		#endregion
+
+		#region Apply Settings
+
+		if (isFirstScene)
+		{
+			if (PlayerPrefs.GetInt("Resolution Quality") == 2)
+			{
+				Screen.SetResolution((int)(PlayerPrefs.GetInt("OriginalX") * 0.5f),
+					(int)(PlayerPrefs.GetInt("OriginalY") * 0.5f), true);
+			}
+			if (PlayerPrefs.GetInt("Resolution Quality") == 1)
+			{
+				Screen.SetResolution((int)(PlayerPrefs.GetInt("OriginalX") * 0.7f),
+					(int)(PlayerPrefs.GetInt("OriginalY") * 0.7f), true);
+			}
+			if (PlayerPrefs.GetInt("Resolution Quality") == 0)
+			{
+				Screen.SetResolution((int)(PlayerPrefs.GetInt("OriginalX") * 1),
+					(int)(PlayerPrefs.GetInt("OriginalY") * 1), true);
+			}
+			
+			QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Quality Level"), false);
+
+		}
+
+		#endregion
+	}
+}
